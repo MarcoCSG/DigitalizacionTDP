a14f3ac21ad1ae53bb68e2b159e27d97b1aae650
MarcoCSG <marcocesarsg@gmail.com>
2024-08-22 16:22:59 -0600

    subir archivos

--- upload.php
+++ upload.php
@@ -3,23 +3,24 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     // Verificar si los campos existen y obtener los valores
     $area = isset($_POST['area']) ? $_POST['area'] : null;
     $clasificacion = isset($_POST['clasificacion']) ? $_POST['clasificacion'] : null;
-    $documentos = isset($_POST['documentos']) ? $_POST['documentos'] : null;
+    $subclasificacion = isset($_POST['documentos']) ? $_POST['documentos'] : null;
     $periodo = isset($_POST['periodo']) ? $_POST['periodo'] : null;
     $cantidad_folios = isset($_POST['cantidad_folios']) ? $_POST['cantidad_folios'] : null;
 
     // Verificar que todos los datos requeridos estén presentes
-    if (!$area || !$clasificacion || !$documentos || !$periodo || !$cantidad_folios) {
+    if (!$area || !$clasificacion || !$subclasificacion || !$periodo || !$cantidad_folios) {
         if (!$area) echo "El campo 'Área' es obligatorio.<br>";
         if (!$clasificacion) echo "El campo 'Clasificación' es obligatorio.<br>";
-        if (!$documentos) echo "El campo 'Documentos' es obligatorio.<br>";
+        if (!$subclasificacion) echo "El campo 'Documentos' es obligatorio.<br>";
         if (!$periodo) echo "El campo 'Periodo' es obligatorio.<br>";
         if (!$cantidad_folios) echo "El campo 'Cantidad de folios' es obligatorio.<br>";
         die("Todos los campos son obligatorios.");
     }
+
     // Directorios específicos por área
     $directorios = [
-        'presidencia' => 'D:/PRESIDENCIA/',
-        'sindicatura' => 'D:/SINDICATURA/',
+        'presidencia' => 'C:/Users/BC/OneDrive/Escritorio/DIGITALIZACION TDP/PRESIDENCIA/',
+        'sindicatura' => 'C:/Users/BC/OneDrive/Escritorio/DIGITALIZACION TDP/SINDICATURA/',
         'secretaria' => 'D:/SECRETARIA/',
         'regidores' => 'D:/REGIDORES/',
         'tesoreria' => 'D:/TESORERIA/',
@@ -57,10 +58,9 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         }
 
         // Preparar la consulta para insertar los datos
-        // El valor de 'documentos' se almacenará en la columna 'clasificacion'
-        $sql = "INSERT INTO `$area` (clasificacion, periodo, nombre_archivo, ruta_archivo, cantidad_folios) VALUES (?, ?, ?, ?, ?)";
+        $sql = "INSERT INTO `$area` (clasificacion, subclasificacion, periodo, nombre_archivo, ruta, cantidad_folios) VALUES (?, ?, ?, ?, ?, ?)";
         $stmt = $conn->prepare($sql);
-        $stmt->bind_param("ssssi", $documentos, $periodo, $nombre_archivo, $targetPath, $cantidad_folios);
+        $stmt->bind_param("sssssi", $clasificacion, $subclasificacion, $periodo, $nombre_archivo, $targetPath, $cantidad_folios);
 
         if ($stmt->execute()) {
             echo "Archivo subido y clasificado exitosamente. Cantidad de folios: " . $cantidad_folios;
